ad5ecab3629d93fd74bccdfe58083b13b86e8080
Ahmedsa3d2025 <ahmedsa3d97.2020@gmail.com>
2025-07-29 01:19:10 +0300

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+		cout << "Enter an integer:";
+		cin >> num;
+
+		if (num % 2 == 0)
+		cout << " The num is even.:" << "\n";
+		else
+		 cout << "the num is odd." << "\n";
+}
